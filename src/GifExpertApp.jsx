5dbf7cec56e90f4175e1524aa15cfb28a4d00a61
LucianoR23 <231097lr@gmail.com>
2023-03-07 12:44:11 -0300

    81 gifgrid, estancado volver a ver

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
+import { AddCattegory } from "./components/AddCattegory";
+import { GifGrid } from "./components/GifGrid";
+
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
 
+    const onAddCategory = (newCategory) => {
+
+      if (categories.includes(newCategory)) return;
+
+      // categories.push('Naruto Shippuden');  EVITAR USAR, SE USA PARA MUTAR UN OBJETO, UN ESTADO
+      // setCategories(['Naruto Shippuden', ...categories])
+      // setCategories(cat => [...cat, 'Naruto Shippuden'])
+      setCategories([newCategory, ...categories])
+    }
+
   return (
     <>
     <h1>GifExpertApp</h1>
-    <ol>
-        {categories.map(category => {
-            return <li key={category}>{category}</li>
-        })}
-    </ol>
+
+    <AddCattegory 
+    // setCategories = {setCategories} 
+    onNewCategory = {(event)=> onAddCategory(event)}
+    />
+
+        {
+          categories.map((category) => (
+                <GifGrid key={category} />
+          ))
+        }
     </>
   )
 }
